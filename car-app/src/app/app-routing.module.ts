a8e42678114f78d4485de399e5217046a5474db2
Yoan <yoan_vt@abv.bg>
2020-02-26 15:15:20 +0200

    add navigation and routing

--- car-app/src/app/app-routing.module.ts
+++ car-app/src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { HomeComponent } from "./home/home.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    redirectTo: "/Home",
+    pathMatch: "full"
+  },
+  {
+    path: "Home",
+    component: HomeComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
